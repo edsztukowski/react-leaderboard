3331b4657c2b9fac410e192777032b983ea2e371
EDWARD <edsztukowski@gmail.com>
2017-07-28 16:33:51 -0600

    pushing monthly data on to state of table

--- src/Table.js
+++ src/Table.js
@@ -2,12 +2,39 @@ var React = require('react');
 var api = require('./utils/api')
 
 class Table extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tableData: ''
+    }
+    this.updateRecent = this.updateRecent.bind(this)
+  }
+
+/*
   componentDidMount() {
     api.callMonth()
     .then(function(response) {
       console.log(response)
     })
   }
+*/
+
+  updateRecent() {
+    this.setState(function() {
+      return {
+        tableData: ''
+      }
+    })
+    api.callMonth()
+    .then(function(response) {
+      this.setState(function() {
+          return {
+            tableData:response
+          }
+        })
+      }.bind(this));
+    }
+
   render() {
     return (
       <div className="table-header col-md-12">
@@ -15,7 +42,7 @@ class Table extends React.Component {
         <div className="table-cells col-md-12">
           <div className="col-md-1">#</div>
           <div className="col-md-5">Camper Name</div>
-          <div className="col-md-3" onClick={api.callMonth}>Points in last 30 days</div>
+          <div className="col-md-3" onClick={this.updateRecent}>Points in last 30 days</div>
           <div className="col-md-3" onSelect={api.callAll}>All Time Points</div>
         </div>
       </div>
