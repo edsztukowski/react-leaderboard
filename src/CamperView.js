f4c226204eef0b0b244756ae6f173b8884434bed
EDWARD <edsztukowski@gmail.com>
2017-07-29 21:32:27 -0600

    Adding images and links and more styling

--- src/CamperView.js
+++ src/CamperView.js
@@ -1,5 +1,4 @@
 var React = require('react');
-var PropTypes = require("prop-types");
 
 function CamperView(props) {
 
@@ -9,11 +8,20 @@ function CamperView(props) {
     <div>
     {camperArr.map(function(curr, index) {
       return (
-        <div className="camper-row">
+        <div className="camper-row" key={curr+index}>
           <div className="col-xs-1" key={index + 1}>{index + 1}</div>
-          <div className="col-xs-5" key={curr.username}>{curr.username}</div>
-          <div className="col-xs-3" key={curr.recent}>{curr.recent}</div>
-          <div className="col-xs-3" key={curr.alltime}>{curr.alltime}</div>
+          <div className="col-xs-5" key={curr.username}>
+            <a target="_blank" href={'https://www.freecodecamp.org/' + curr.username}>
+              <img
+                className="user-image"
+                src={curr.img}
+                alt={curr.username}
+              />
+              {curr.username}
+            </a>
+          </div>
+          <div className="col-xs-3" key={curr.username + curr.recent + index}>{curr.recent}</div>
+          <div className="col-xs-3" key={curr.username + curr.alltime}>{curr.alltime}</div>
         </div>
       )
     }, this)}
